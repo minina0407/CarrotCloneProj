3af2443da987db5bb7a351a2682616ea1f20212e
minina0407 <minina@kyonggi.ac.kr>
2022-05-09 18:59:27 +0900

    init

---
+++ src/main/java/com/example/carrotproj/CarrotProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.carrotproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrotProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarrotProjApplication.class, args);
+    }
+
+}
